d54cdee1d32b4ccb3eaae3cf60f8aaa13a1abbaf
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2020-01-24 15:37:43 +0100

    Adds private constructor example, to guarantee class noninstantiability

---
+++ src/main/java/com/abevilacqua/item4_privateConstructors/UtilityClass.java
@@ -0,0 +1,11 @@
+package com.abevilacqua.item4_privateConstructors;
+
+import java.io.InvalidObjectException;
+
+// Noninstantiable utility class
+public class UtilityClass {
+
+  private UtilityClass() {
+    // Suppress default constructor for noninstantiability
+  }
+}
