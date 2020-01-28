bed8a0994574db20417589b4ac4af98a8f06cd53
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2020-01-28 16:47:54 +0100

    Adds item 6 example about expensive objects

---
+++ src/main/java/com/abevilacqua/item6_avoidCreateUnnecessaryObjects/RomanNumerals.java
@@ -0,0 +1,18 @@
+package com.abevilacqua.item6_avoidCreateUnnecessaryObjects;
+
+import java.util.regex.Pattern;
+
+public class RomanNumerals {
+
+  private static final Pattern ROMAN = Pattern.compile("^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$");
+
+  // Performance can be greatly improved
+  static boolean isRomanNumeral(String s) {
+    return s.matches("^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$");
+  }
+
+  // Reusing expensive object for improved performance
+  static boolean isRomanNumeral2(String s) {
+    return ROMAN.matcher(s).matches();
+  }
+}
