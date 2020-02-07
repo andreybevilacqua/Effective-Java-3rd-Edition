1bd83149e49649d5f9dca4f36f534eb251f5ffc0
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2020-02-07 14:55:07 +0100

    Removes throw

--- src/main/java/com/abevilacqua/item9_preferTryWithResources/FileReader.java
+++ src/main/java/com/abevilacqua/item9_preferTryWithResources/FileReader.java
@@ -7,7 +7,7 @@ public class FileReader {
   private static final int BUFFER_SIZE = 16;
   private static String defaultVal = "DEFAULT_VALUE";
   // try-with-resources - the best way to close resources
-  static String firstLineOfFile(String path) throws IOException {
+  static String firstLineOfFile(String path) {
     try (BufferedReader br = new BufferedReader(new java.io.FileReader(path))) {
       return br.readLine();
     } catch (IOException e) {
