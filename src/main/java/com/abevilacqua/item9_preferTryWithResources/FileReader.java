c2aba333660e00e3468b066a030fd0e4b744255c
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2020-02-07 14:54:21 +0100

    Adds catch

--- src/main/java/com/abevilacqua/item9_preferTryWithResources/FileReader.java
+++ src/main/java/com/abevilacqua/item9_preferTryWithResources/FileReader.java
@@ -5,11 +5,14 @@ import java.io.*;
 public class FileReader {
 
   private static final int BUFFER_SIZE = 16;
-
+  private static String defaultVal = "DEFAULT_VALUE";
   // try-with-resources - the best way to close resources
   static String firstLineOfFile(String path) throws IOException {
     try (BufferedReader br = new BufferedReader(new java.io.FileReader(path))) {
       return br.readLine();
+    } catch (IOException e) {
+
+      return defaultVal;
     }
   }
 
